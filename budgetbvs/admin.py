8e7a7b20a82c74ccc11f5d15142b2e3446d52307
abe27 <taweechai.yuenyang@outlook.com>
2024-02-29 14:47:33 +0700

    test

--- budgetbvs/admin.py
+++ budgetbvs/admin.py
@@ -223,10 +223,10 @@ class LogStepSendMailBVSAdmin(admin.ModelAdmin):
             #     LogStepSendMailBVS.objects.filter(
             #         RefNo=object.RefNo, StepID=object.StepID).delete()
             try:
-                LogStepSendMailBVS.objects.filter(RefNo=object.RefNo, StepID__gte=stepTotal).delete()
+                LogStepSendMailBVS.objects.filter(RefNo=object.RefNo).delete()
             except Exception as e:
                 print(e)
-                
+
             urls = f"http://110.164.218.143:11227/web_sevice_bvs/web_Approve.aspx?EMP_ID={(prHead.FCCREATEBY).strip()}&P={(object.RefNo).strip()}&STEP={stepTotal}&BOOK=1"
             return HttpResponseRedirect(urls)
         
